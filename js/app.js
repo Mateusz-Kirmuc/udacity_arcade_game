8f1fe07dfee7ac3f28ec14d3771e804abeff587f
Mateusz <mateusz.kirmuc@gmail.com>
2017-10-20 10:29:17 +0200

    Implement Player constructor function

--- js/app.js
+++ js/app.js
@@ -28,6 +28,16 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+  this.sprite = 'images/char-boy.png';
+  this.x = x;
+  this.y = y;
+  this.dx = 0;
+  this.dy = 0;
+  console.log("x: " + this.x);
+  console.log("y: " + this.y);
+};
+
 
 
 // Now instantiate your objects.
